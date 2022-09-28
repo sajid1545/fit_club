9b6ed372e41bb5eb6be0112b5e27bd85300a66ed
sajid9962 <clashking1545@gmail.com>
2022-09-28 21:55:37 +0600

    cards displayed dynamically

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,14 +1,25 @@
 import React from 'react';
 
 const Card = ({ exercise }) => {
-    
-    const { title,picture,time,age } = exercise;
+	const { title, picture, time, age } = exercise;
 
-    return (
-        <div>
-            
-        </div>
-    );
+	return (
+		<div className=''>
+			<div className="card card-compact  bg-base-100 shadow-xl">
+				<figure>
+					<img src={picture} alt="pic" />
+				</figure>
+				<div className="card-body">
+                    <h2 className="card-title">{ title }</h2>
+                    <h2>For Age : { age }</h2>
+                    <h2>Time Required : { time}s</h2>
+					<div className="card-actions">
+						<button className="btn btn-primary w-full">Add To List</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Card;
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -5,22 +5,18 @@ import Card from '../Card/Card';
 import List from '../List/List';
 
 const Home = () => {
+	const [exercises, setExercises] = useState([]);
 
-    const [exercises,setExercises] = useState([])
-
-    useEffect(() => { 
-        fetch('data.json')
-            .then(res => res.json())
-        .then(data => setExercises(data))
-     }, [])
-
-
+	useEffect(() => {
+		fetch('data.json')
+			.then((res) => res.json())
+			.then((data) => setExercises(data));
+	}, []);
 
 	return (
-        <div className=" grid grid-cols-12">
-            
+		<div className=" grid grid-cols-12">
 			{/* cards container */}
-			<div className='col-span-9 px-[80px] py-[70px]'>
+			<div className="col-span-9 px-[80px] py-[70px]">
 				<div className="text-4xl text-purple-700 flex gap-5 ">
 					<FontAwesomeIcon className="font-extrabold" icon={faDumbbell}></FontAwesomeIcon>
 					<h1 className="font-bold">My FitnessPal</h1>
@@ -28,19 +24,20 @@ const Home = () => {
 
 				<div>
 					<h1 className="text-2xl mt-10 font-semibold">Start Today's Exercise</h1>
-                </div>
-                {
-                    exercises.map(exercise => <Card key={exercise.id} exercise={ exercise }></Card>)
-                }
+				</div>
+
+				<div className='grid grid-cols-3 gap-5 mt-5'>
+					{exercises.map((exercise) => (
+						<Card key={exercise.id} exercise={exercise}></Card>
+					))}
+				</div>
 			</div>
 
 			{/* list container */}
 
-			<div className='col-span-3 bg-white py-10 px-5'>
+			<div className="col-span-3 bg-white py-10 px-5">
 				<List></List>
-            </div>
-            
-
+			</div>
 		</div>
 	);
 };
